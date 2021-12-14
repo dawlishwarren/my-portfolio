89531b657b7e5f6819773a805a556e969a4db053
dawlishwarren <dawlishwarren@gmail.com>
2021-12-14 08:55:01 +0000

    .next added

---
+++ .next/static/webpack/pages/_app.a66855f0190e732d.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./styles/themes.json":
+/*!****************************!*\
+  !*** ./styles/themes.json ***!
+  \****************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('{"data":{"light":{"index":"0","id":"t_001","name":"lightTheme","title":"Light","colors":{"body":"#f8f8f8","text":"#2a2e35","navigation":{"background":"linear-gradient(to right, #f8f8f8, #6c7983)","dropdown":"#b2becd"},"border":"#12181b","accent":{"primary":"#cc4f1f","secondary":"#bdc1cc"}}},"dark":{"index":"1","id":"t_002","colors":{"body":"#2a2e35","text":"#f8f8f8","navigation":{"background":"linear-gradient(to right, #454e56, #12181b)","dropdown":"#12181b"},"border":"#dbe1e8","accent":{"primary":"#3700b3","secondary":"#FF4C29"}},"name":"darkTheme","title":"Dark"},"sepia":{"index":"2","id":"t_003","colors":{"body":"#cda882","text":"#eadbcb","navigation":{"background":"linear-gradient(to right, #454e56, #12181b)","dropdown":"#cda882"},"border":"#eadbcb","accent":{"primary":"#332718","secondary":"#7D803C"}},"name":"sepiaTheme","title":"Sepia"}}}');
+
+/***/ })
+
+});
---
+++ pages/portfolio.js
@@ -0,0 +1,9 @@
+import Layout from "./components/layout/layout";
+
+export default function Portfolio() {
+	return (
+		<Layout inner>
+			<h1>My Portfolio</h1>
+		</Layout>
+	);
+}
